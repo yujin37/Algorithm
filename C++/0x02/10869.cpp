b2dfea1e50434bfb28f6eb3a5dd88f69f2eec3ce
yujinni <86652449+yujin37@users.noreply.github.com>
2022-02-22 12:12:21 +0900

    Create 10869.cpp

---
+++ C++/0x02/10869.cpp
@@ -0,0 +1,16 @@
+// 사칙연산
+#include <bits/stdc++.h>//자주 사용 라이브러리 전부 컴파일
+using namespace std;//std 네임 스페이스를 사용
+
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int a, b;
+  cin >> a >> b;
+  cout << a+b<<"\n";
+  cout << a-b<<"\n";
+  cout << a*b<<"\n";
+  cout << a/b<<"\n";
+  cout << a%b<<"\n";
+}
