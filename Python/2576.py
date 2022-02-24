f5096daed1f9d3031e3018be5603951d77883851
yujinni <86652449+yujin37@users.noreply.github.com>
2022-02-24 13:05:51 +0900

    Create 2576.py

---
+++ Python/2576.py
@@ -0,0 +1,11 @@
+#홀수
+list=[]
+for i in range(7):
+    n=int(input())
+    if(n%2!=0):
+        list.append(n)
+if(len(list)!=0): 
+    print(sum(list)) 
+    print(min(list))
+else:
+    print("-1")
