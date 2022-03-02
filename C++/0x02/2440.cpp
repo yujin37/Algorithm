39fa2e27719f88fced1f24dca377371487681ea9
yujinni <86652449+yujin37@users.noreply.github.com>
2022-03-02 12:02:04 +0900

    Create 2440.cpp

---
+++ C++/0x02/2440.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>//자주 사용 라이브러리 전부 컴파일
+using namespace std;//std 네임 스페이스를 사용
+//별찍기3
+
+int main(void){
+  int a;
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin>>a;
+  for(int i=0;i<a;i++){
+    for(int j=1;j<=a-i;j++){
+      cout<<"*";
+    }
+    cout<<"\n";
+  }
+}
