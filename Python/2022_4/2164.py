92813dfe8db3dda3cccf48686113d8c9abb590c1
yujinni <86652449+yujin37@users.noreply.github.com>
2022-04-17 14:23:30 +0900

    Update 2164.py

--- Python/2022_4/2164.py
+++ Python/2022_4/2164.py
@@ -2,9 +2,10 @@
 import sys
 import collections
 n=int(sys.stdin.readline())#정수 주어지기
-card=collections.deque([i for i in range(n)])
+card=collections.deque([i+1 for i in range(n)])
 while(len(card)>1):
-    del card[0]
+    print(card)
+    card.popleft()
     num=card.popleft()
     card.append(num)
-print(card[0]+1)
+print(card[0])
