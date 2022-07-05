9bc8df28e00d9008395d7aebe62b72bc8fdde774
yujinni <86652449+yujin37@users.noreply.github.com>
2022-07-05 15:56:56 +0900

    Create 1920.py

---
+++ Python/2022_7/1920.py
@@ -0,0 +1,20 @@
+import sys
+n1=sys.stdin.readline()
+num1=sorted(map(int,sys.stdin.readline().split()))
+n2=sys.stdin.readline()
+num2=map(int,sys.stdin.readline().split())
+
+def binary(i,num1,start,end):
+    if start>end:
+        return 0
+    m=(start+end)//2
+    if i == num1[m]:
+        return 1
+    elif i<num1[m]:
+        return binary(i,num1,start,m-1)
+    else:
+        return binary(i,num1,m+1,end)
+for i in num2:
+    start=0
+    end=len(num1)-1
+    print(binary(i,num1, start,end))
