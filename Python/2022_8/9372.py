6066ae2ac379957bf7b7dcea6279cbb64e6c9be7
yujinni <86652449+yujin37@users.noreply.github.com>
2022-08-31 12:15:30 +0900

    Create 9372.py

---
+++ Python/2022_8/9372.py
@@ -0,0 +1,11 @@
+#실버4 상근이의 여향
+import sys
+input=sys.stdin.readline
+
+t=int(input())
+
+for i in range(t):
+    n,m=map(int,input().split())
+    for j in range(m):
+        input()
+    print(n-1)
