afdceb939cd4e6cea03ca2b03e8e9e4b664d1b97
yujinni <86652449+yujin37@users.noreply.github.com>
2022-09-15 16:56:14 +0900

    Create 하샤드수.py

---
+++ Python/2022_9/하샤드수.py
@@ -0,0 +1,15 @@
+def solution(x):
+    sum_num=0
+    for i in str(x):
+        sum_num+=int(i)
+    if x%sum_num==0:
+        return True
+    else:
+        return Falsedef solution(x):
+    sum_num=0
+    for i in str(x):
+        sum_num+=int(i)
+    if x%sum_num==0:
+        return True
+    else:
+        return False
