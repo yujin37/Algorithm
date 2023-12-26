ac2411e08a27f2e762ba728ddae38cb5e672d6a4
yujin37 <yj061628@gmail.com>
2023-12-26 11:12:38 +0900

    TIL_231209

---
+++ Python/book_Python_Algorithm_InterView/125.py
@@ -0,0 +1,12 @@
+from collections import deque
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        strs = deque()
+        for char in s:
+            if char.isalnum():
+                strs.append(char.lower())
+            
+        while len(strs) > 1:
+            if strs.popleft() != strs.pop():
+                return False
+        return True
