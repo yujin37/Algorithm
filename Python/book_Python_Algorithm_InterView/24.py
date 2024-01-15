cd3731591443867066a8956472f0954c9f84b171
yujin37 <yj061628@gmail.com>
2024-01-15 22:17:31 +0900

    TIL_240115

---
+++ Python/book_Python_Algorithm_InterView/24.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# 값 바꾸는 형태만 생각함. 해당 풀이는 예시풀이를 보고 이해함.
+from typing import Optional
+from typing import ListNode
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        root = prev = ListNode(None)
+        prev.next = head
+
+        while head and head.next:
+            b = head.next
+            head.next = b.next
+            b.next = head
+
+            prev.next = b
+
+            head = head.next
+            prev = prev.next.next
+        return root.next
+        
