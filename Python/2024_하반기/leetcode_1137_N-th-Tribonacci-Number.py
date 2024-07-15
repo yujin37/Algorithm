004d22768f6a52c97900fd2905f70e94f373936c
yujinni <86652449+yujin37@users.noreply.github.com>
2024-07-15 16:21:36 +0900

    TIL_240715

---
+++ Python/2024_하반기/leetcode_1137_N-th-Tribonacci-Number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n == 0:
+            return 0
+        elif n == 1 or n == 2:
+            return 1
+        a, b, c = 0, 1, 1
+
+        for i in range(3, n+1):
+            new_num = a + b + c
+            a = b
+            b = c
+            c = new_num
+        return c
